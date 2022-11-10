8e894f95685fcce61fb61871c57ce6b594419973
CaioDoro <117940103+CaioDoro@users.noreply.github.com>
2022-11-10 16:31:56 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,22 @@ using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
-
+  
+float Atriangolo, Aquadrato, Arettangolo, Atrapezio, a, b, c;
 cin >> a >> b >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
 
+    Atriangolo = (a * b) / 2;
+    Aquadrato = a * a;
+    Arettangolo = a * b;
+    Atrapezio = ((a + b) * c) / 2;
+
+    cout << " " << Atriangolo << endl;
+    cout << " " << Aquadrato << endl;
+    cout << " " << Arettangolo << endl;
+    cout << " " << Atrapezio << endl;
+  
+  return 0;
 
 }
